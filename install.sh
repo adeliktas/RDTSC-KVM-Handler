f462488522036d8cc6b783a95c6ce1e74adc45f5
adeliktas <64136642+adeliktas@users.noreply.github.com>
2021-11-15 15:01:40 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 Kernel_Path=/usr/src/linux/
 #backup
-cp $Kernel_Path/arch/x86/kvm/vmx/vmx.c $Kernel_Path/arch/x86/kvm/vmx/orig_vmx.c
-cp $Kernel_Path/arch/x86/kvm/svm/svm.c $Kernel_Path/arch/x86/kvm/svm/orig_svm.c
+mv $Kernel_Path/arch/x86/kvm/vmx/vmx.c $Kernel_Path/arch/x86/kvm/vmx/orig_vmx.c
+mv $Kernel_Path/arch/x86/kvm/svm/svm.c $Kernel_Path/arch/x86/kvm/svm/orig_svm.c
 #replace
 cp ./Linux\ kernel/vmx.c $Kernel_Path/arch/x86/kvm/vmx/.
 cp ./Linux\ kernel/svm.c $Kernel_Path/arch/x86/kvm/svm/.
